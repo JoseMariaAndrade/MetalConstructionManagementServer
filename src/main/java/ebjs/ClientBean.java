47ce90de2ed4f586ac8b8b373663468feb3e2b9f
José Maria Marques Andrade <JoseMariaAndrade@users.noreply.github.com>
2020-10-27 19:36:36 +0000

    Create, Read, Update and Delete Client

--- src/main/java/ebjs/ClientBean.java
+++ src/main/java/ebjs/ClientBean.java
@@ -1,10 +1,16 @@
 package ebjs;
 
 import entities.Client;
+import exceptions.MyConstraintViolationException;
+import exceptions.MyEntityExistsException;
+import exceptions.MyEntityNotFoundException;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.LockModeType;
 import javax.persistence.PersistenceContext;
+import javax.validation.ConstraintViolationException;
+import java.util.List;
 
 @Stateless
 public class ClientBean {
@@ -12,9 +18,61 @@ public class ClientBean {
     @PersistenceContext
     EntityManager entityManager;
 
-    public void create(String name, String email) {
+    public void create(String name, String email, String contact, String address)
+            throws MyEntityExistsException, MyConstraintViolationException {
 
-        Client client = new Client(name, email);
-        entityManager.persist(client);
+        Client client = findClient(name);
+
+        if (client != null)
+            throw new MyEntityExistsException("");
+
+        try{
+            client = new Client(name, email, contact, address);
+            entityManager.persist(client);
+
+        } catch (ConstraintViolationException constraintViolationException) {
+            throw new MyConstraintViolationException(constraintViolationException);
+        }
+
+    }
+
+    public List<Client> getAll() {
+        return entityManager.createNamedQuery("getAllClients").getResultList();
+    }
+
+    public Client findClient(String name) {
+        return entityManager.find(Client.class, name);
+    }
+
+    public void update(String name, String email, String contact, String address)
+            throws MyEntityNotFoundException, MyConstraintViolationException {
+
+        Client client = findClient(name);
+
+        if (client == null) {
+            throw new MyEntityNotFoundException("");
+        }
+
+        try {
+            entityManager.lock(client, LockModeType.OPTIMISTIC);
+            client.setEmaill(email);
+            client.setContact(contact);
+            client.setMorada(address);
+
+        } catch (ConstraintViolationException constraintViolationException) {
+            throw new MyConstraintViolationException(constraintViolationException);
+        }
+
+    }
+
+    public void delete(String name)
+            throws MyEntityNotFoundException {
+
+        Client client = findClient(name);
+
+        if (client == null)
+            throw new MyEntityNotFoundException("");
+
+        entityManager.remove(client);
     }
 }
--- src/main/java/entities/Client.java
+++ src/main/java/entities/Client.java
@@ -1,10 +1,7 @@
 package entities;
 
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
@@ -12,10 +9,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@NamedQueries({
+        @NamedQuery(name = "getAllClients", query = "SELECT c FROM Client c ORDER BY c.name") //JPQL
+})
 public class Client extends User implements Serializable {
 
+    @NotNull
+    private String contact;
+    @NotNull
+    private String morada;
     @OneToMany(mappedBy = "client")
     private List<Project> projects;
+    @Version
+    private int version;
 
     public Client() {
         super();
@@ -24,5 +30,44 @@ public class Client extends User implements Serializable {
     public Client(@NotNull String name, @NotNull @Email String email) {
         super(name, email);
         this.projects = new ArrayList<>();
+    }
+
+    public Client(@NotNull String name, @NotNull @Email String emaill, @NotNull String contact, @NotNull String morada) {
+        super(name, emaill);
+        this.contact = contact;
+        this.morada = morada;
+        this.projects = new ArrayList<>();
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getMorada() {
+        return morada;
+    }
+
+    public void setMorada(String morada) {
+        this.morada = morada;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
     }
 }
